0072a2a75fa3536afe43ca4436db35a7e1750621
Andryut Huertas <andryuthuertas@MacBook-Pro.local>
2021-04-12 12:08:19 -0500

    Login view creation

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,1 +1,70 @@
-//TODO: a basic authentication view using email and password
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      hintText: 'Enter valid email id as abc@gmail.com'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 15, right: 15, top: 15, bottom: 0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter secure password'),
+                ),
+              ),
+              Container(
+                child: TextButton(
+                  onPressed: () {
+                    //TODO: Create a view for this and push it here
+                  },
+                  child: Text(
+                    'Forgot Password',
+                    style: TextStyle(color: Colors.blue, fontSize: 15),
+                  ),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: () {
+                    //TODO: push Main View on Navigator if the backend request is OK
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
