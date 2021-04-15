7f17a9cc0df0764925d58fa50a79f9e283491f0d
Andryut Huertas <andryuthuertas@MacBook-Pro.local>
2021-04-14 23:56:38 -0500

    views added

--- lib/util/routes.dart
+++ lib/util/routes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../views/login_view.dart';
+import '../views/main_menu_view.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   '/login': (context) => LoginView(),
+  '/main-menu': (context) => MainMenuView(),
 };
--- lib/views/main_menu_view.dart
+++ lib/views/main_menu_view.dart
@@ -1,1 +1,36 @@
-//TODO: the main view receives a body widget and a side nav menu
+import 'package:flutter/material.dart';
+
+class MainMenuView extends StatefulWidget {
+  @override
+  _MainMenuViewState createState() => _MainMenuViewState();
+}
+
+class _MainMenuViewState extends State<MainMenuView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Go')),
+      body: Center(child: Text('NFC Verification')),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Go'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Me'),
+              onTap: () => Navigator.pop(context),
+            ),
+            ListTile(
+              title: Text('Logout'),
+              onTap: () => Navigator.pop(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
