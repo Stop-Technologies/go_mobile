54ae47735f9cd5c4249f901e3b8405d027d77188
Alejandro Melo <dmelo@unal.edu.co>
2021-05-05 12:36:53 -0500

    Adding the final fixes to access view ux
    Changes:
    - Adding the correction of the view from mockups
    - Adding functionality to change between allowed and denied access

--- lib/util/routes.dart
+++ lib/util/routes.dart
@@ -1,14 +1,14 @@
-//import 'dart:js';
-
+// Flutter imports;
 import 'package:flutter/material.dart';
 import 'package:go_mobile/views/access_view.dart';
 import 'package:go_mobile/views/nfc_view.dart';
+
+// Project imports
 import '../views/login_view.dart';
 import '../views/main_menu_view.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   '/login': (context) => LoginView(),
   '/main-menu': (context) => MainMenuView(),
-  '/nfc-view': (context) => NFCView(),
-  '/access-view': (context) => AccessView()
+  '/nfc-view': (context) => NFCView()
 };
