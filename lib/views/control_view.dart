5c8cceda636c380962ed455d3cddfb4a946cfada
Alejandro Melo <dmelo@unal.edu.co>
2021-07-10 07:56:24 -0500

    Adding preliminar control view buttons display
    Changes:
    - Adding button layout for control view

--- lib/views/control_view.dart
+++ lib/views/control_view.dart
@@ -14,6 +14,62 @@ class ControlView extends StatefulWidget {
 class _ControlViewState extends State<ControlView> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: appColors.lightBlue);
+    return Container(
+        color: appColors.lightBlue,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 60, 20, 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: appColors.darkBlue,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: TextButton(
+                      onPressed: _onPressed,
+                      child: Text('Lugares',
+                          style: TextStyle(color: appColors.white))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: appColors.darkBlue,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: TextButton(
+                      onPressed: _onPressed,
+                      child: Text('Permisos',
+                          style: TextStyle(color: appColors.white))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: appColors.darkBlue,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: TextButton(
+                      onPressed: _onPressed,
+                      child: Text('Invitados',
+                          style: TextStyle(color: appColors.white))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 40, 20, 60),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: appColors.darkBlue,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: TextButton(
+                      onPressed: _onPressed,
+                      child: Text('Usuarios',
+                          style: TextStyle(color: appColors.white))),
+                ),
+              )
+            ]));
   }
+
+  void _onPressed() {}
 }
